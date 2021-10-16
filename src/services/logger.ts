07514d6b2c2089aedac81e615a001578d1062cf8
WitecuseDev <witecuse@gmail.com>
2021-10-16 12:12:09 -0700

    update code quality

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,14 +1,14 @@
 import { createLogger, format, transports } from "winston";
 
-const logsFolder: string = process.env.NODE_ENV === "production " ? "logs/production" : "logs/development";
+const logsPath: string = process.env.NODE_ENV === "production " ? "logs/production" : "logs/development";
 
 export const Logger = createLogger({
     level: "info",
     format: format.json(),
     transports: [
-        new transports.File({ filename: `${logsFolder}/error.log`, level: "error" }),
-        new transports.File({ filename: `${logsFolder}/warn.log`, level: "warn" }),
-        new transports.File({ filename: `${logsFolder}/combined.log` }),
+        new transports.File({ filename: `${logsPath}/error.log`, level: "error" }),
+        new transports.File({ filename: `${logsPath}/warn.log`, level: "warn" }),
+        new transports.File({ filename: `${logsPath}/combined.log` }),
     ],
 });
 
