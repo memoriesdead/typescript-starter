3d7c960211a2f2f336f04eefca5d008f59485085
WitecuseDev <witecuse@gmail.com>
2021-10-22 11:34:58 -0700

    add eslint

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from "winston";
 
-const logsPath: string = process.env.NODE_ENV === "production " ? "logs/production" : "logs/development";
+const logsPath: string =
+    process.env.NODE_ENV === "production " ? "logs/production" : "logs/development";
 
 export const Logger = createLogger({
     level: "info",
@@ -8,14 +9,14 @@ export const Logger = createLogger({
     transports: [
         new transports.File({ filename: `${logsPath}/error.log`, level: "error" }),
         new transports.File({ filename: `${logsPath}/warn.log`, level: "warn" }),
-        new transports.File({ filename: `${logsPath}/combined.log` })
-    ]
+        new transports.File({ filename: `${logsPath}/combined.log` }),
+    ],
 });
 
 if (process.env.NODE_ENV !== "production") {
     Logger.add(
         new transports.Console({
-            format: format.simple()
+            format: format.simple(),
         })
     );
 }
